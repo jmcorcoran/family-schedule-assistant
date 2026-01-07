b894bd052ded9768e2893fbaf8cdb9f4d2a9ee0c
jmcorcoran <jmcorcoran@gmail.com>
2026-01-07 16:54:38 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/family-schedule-assistant/', // ⚠️ CHANGE THIS if your GitHub repo will have a different name
+  base: '/family-schedule-assistant/',
   build: {
     outDir: 'dist',
   },
